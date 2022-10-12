4944dfc21e8672ff5797c86ce596af352e85797c
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-10-12 00:36:32 -0500

    server side proprs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {PrismaClient} from '@prisma/client'
 
-
-export default function Home() {
+export default function Home({categorias}) {
+  console.log(categorias)
   return (
     <div >
       <h1>Next.js + Tailwind CSS</h1>
@@ -10,4 +11,17 @@ export default function Home() {
       
     </div>
   )
+}
+
+
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+  const categorias = await prisma.categoria.findMany();
+  // console.log(categorias)
+  return {
+    props: {
+      categorias
+    }
+  }
 }
