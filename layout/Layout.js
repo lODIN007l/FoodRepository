87dc7c5bc61e0187c7a76150400d473921299749
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-10-17 23:51:39 -0500

    correciones estilos

--- layout/Layout.js
+++ layout/Layout.js
@@ -9,10 +9,12 @@ const Layout = ({ children, pagina }) => {
         <meta name="descripcion " content="Quisko Cafeteria" />
       </Head>
       <div className="md:flex">
-        <aside className="md:w-4/12">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5 ">
           <h1>Sidebar aqui </h1>
         </aside>
-        <main className="md:w-8/12">{children}</main>
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
+          {children}
+        </main>
       </div>
     </>
   );
